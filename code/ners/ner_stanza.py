1b9ba17b42b6ead064ee3ba66b0974427b9ef70d
Žan Pečovnik <zp8358@student.uni-lj.si>
2023-04-22 15:28:36 +0200

    Add stanza sentiment analysis

--- code/ners/ner_stanza.py
+++ code/ners/ner_stanza.py
@@ -1,22 +1,15 @@
-# pip install stanza
-
-import stanza
 import sys
 
 sys.path.append('..')
 
 from helpers.helper_cr import perform_cr
 
-stanza.download('en')
-stanza_nlp = stanza.Pipeline('en')
-
-def ner_stanza(story, use_cr=False):
+def ner_stanza(story, stanza_nlp, use_cr=False):
   if use_cr == True:
     story = perform_cr(story)
 
   doc = stanza_nlp(story)
-  entities = list(set([ent.text.lower() for ent in filter(lambda e: e.type == "PERSON", doc.ents)]))
-  return entities
+  return doc
 
 
 def test_ner_stanza():
@@ -26,5 +19,3 @@ def test_ner_stanza():
     represented Delaware in the United States Senate from 1973 to 2009."
     print("Entities without CR", ner_stanza(text))
     print("Entities with CR", ner_stanza(text, use_cr=True))
-
-test_ner_stanza()
---
+++ code/stanza_sentiment.py
@@ -0,0 +1,53 @@
+import stanza
+import pandas as pd
+from ners.ner_stanza import ner_stanza
+
+def sentiment_mapping(sentiment_value):
+    if sentiment_value == 0:
+        return -1
+    elif sentiment_value == 1:
+        return 0
+    else:
+        return 1
+
+def get_person_df(persons):
+    rows = []
+    for person in persons:
+        row = {
+            "text": person.text,
+            "type": person.type,
+            "start_char": person.start_char,
+            "end_char": person.end_char,
+            "sentence_sentiment": sentiment_mapping(person._sent.sentiment)
+        }
+        rows.append(row)
+
+    return pd.DataFrame(rows)
+
+def read_story_from_file(file_name):
+    with open(f'../data/stories/{file_name}.txt', encoding="utf8") as f:
+        return f.read()
+
+def get_person_entities(doc):
+    return list(filter(lambda e: e.type == "PERSON", doc.entities))
+
+def perform_sentiment(df):
+    sentiment = df.copy()
+    sentiment = sentiment.drop(columns=["type", "start_char", "end_char"])
+    sentiment = sentiment.groupby("text").sum().reset_index()
+
+    return sentiment.sort_values("sentence_sentiment")
+
+if __name__ == "__main__":
+    stanza.download('en')
+    stanza_nlp = stanza.Pipeline('en')
+
+    story = read_story_from_file(file_name="007")
+    
+    doc = ner_stanza(story, stanza_nlp, use_cr=True)
+
+    person_df = get_person_df(get_person_entities(doc))
+
+    sentiment_df = perform_sentiment(person_df)
+
+    print(sentiment_df.head())
