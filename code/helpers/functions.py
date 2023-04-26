596cc3282141f472fbcef2cf23eed01178f35c79
Nace Gorenc <nace.gorenc@outfit7.com>
2023-04-26 23:07:57 +0200

    neki neki

--- code/helpers/functions.py
+++ code/helpers/functions.py
@@ -1,4 +1,7 @@
 import json
+from Levenshtein import jaro
+import os
+import re
 
 
 def read_story_from_file(file_name):
@@ -8,4 +11,73 @@ def read_story_from_file(file_name):
 
 def get_characters_from_file(file_name):
     with open(f"../data/annotations/{file_name}.json", encoding="utf8") as f:
-        return json.load(f)
+        return json.load(f)["characters"]
+
+
+def get_file_names():
+    path = "../data/stories/"
+    file_names = os.listdir(path)
+    file_names = sorted([re.sub(r"\.txt$", "", file_name) for file_name in file_names])
+    return file_names
+
+
+def string_similarity(s1, s2, threshold=0.5):
+    return jaro(s1, s2) >= threshold
+
+
+def true_positive(list1, list2):
+    tp = 0
+    for x in list1:
+        for y in list2:
+            if string_similarity(x, y):
+                tp += 1
+                break
+    return tp
+
+
+def percision_score(list1, list2):
+    try:
+        return true_positive(list1, list2) / len(list1)
+    except ZeroDivisionError:
+        return 1
+
+
+def recall_score(list1, list2):
+    return true_positive(list1, list2) / len(list2)
+
+
+def f1_score(list1, list2):
+    per = percision_score(list1, list2)
+    recall = recall_score(list1, list2)
+    try:
+        return 2 * per * recall / (per + recall)
+    except ZeroDivisionError:
+        return 0
+
+
+def overall_scores(flair=None, stanza=None, spacy=None):
+    p_scores, r_scores, f1_scores = [], [], []
+
+    if flair == None and stanza == None and spacy == None:
+        return None
+
+    for name in get_file_names():
+        story = read_story_from_file(name)
+        characters = get_characters_from_file(name)
+
+        if flair != None:
+            pred = flair.ner_flair(story)
+        elif stanza != None:
+            pred = "vnesi da se bo nardila stanza"
+        elif spacy != None:
+            pred = "vnesi da se bo naredu spacy"
+
+        p_scores.append(percision_score(pred, characters))
+        r_scores.append(recall_score(pred, characters))
+        f1_scores.append(f1_score(pred, characters))
+
+    return (
+        sum(p_scores) / len(p_scores),
+        sum(r_scores) / len(r_scores),
+        sum(f1_scores) / len(f1_scores),
+    )
--- code/ners/ner_flair.py
+++ code/ners/ner_flair.py
@@ -1,32 +1,34 @@
 from flair.data import Sentence
 from flair.nn import Classifier
 import sys
+import re
 
 sys.path.append("..")
 
 from helpers.helper_cr import perform_cr
 
 
-# story - text (whole story) (string)
-# use_cr - use coreference resolution (boolean)
-def ner_flair(story, use_cr=False):
-    tagger = Classifier.load("ner")
+class FlairNer:
+    def __init__(self):
+        self.tagger = Classifier.load("ner")
 
-    sentences = [s.strip() for s in re.split(r"(?<=[.?!])\s+", story)]
-    entities = set()
-    for s in sentences:
-        sentence = Sentence(s)
-        tagger.predict(sentence)
-        for entity in sentence.get_spans("ner"):
-            if entity.tag == "PER":
-                entities.add(entity.text)
-    return list(entities)
+    def ner_flair(self, story, use_cr=False):
+        if use_cr == True:
+            story = perform_cr(story)
+        sentences = [s.strip() for s in re.split(r"(?<=[.?!])\s+", story)]
+        entities = set()
+        for s in sentences:
+            sentence = Sentence(s)
+            self.tagger.predict(sentence)
+            for entity in sentence.get_spans("ner"):
+                if entity.tag == "PER":
+                    entities.add(entity.text)
+        return list(entities)
 
-
-def test_ner_flair():
-    text = "Joseph Robinette Biden Jr. is an American politician who is the 46th and\
-  current president of the United States. A member of the Democratic Party, \
-  he served as the 47th vice president from 2009 to 2017 under Barack Obama and\
-  represented Delaware in the United States Senate from 1973 to 2009."
-    print("Entities without CR", ner_flair(text))
-    print("Entities with CR", ner_flair(text, use_cr=True))
+    def test_ner_flair():
+        text = "Joseph Robinette Biden Jr. is an American politician who is the 46th and\
+    current president of the United States. A member of the Democratic Party, \
+    he served as the 47th vice president from 2009 to 2017 under Barack Obama and\
+    represented Delaware in the United States Senate from 1973 to 2009."
+        print("Entities without CR", ner_flair(text))
+        print("Entities with CR", ner_flair(text, use_cr=True))
