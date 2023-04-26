99a58a4ce47d4cfbc50fb587d545ca6ab418ff27
Nace Gorenc <nace.gorenc@outfit7.com>
2023-04-26 19:21:56 +0200

    ADD: flair

--- code/ners/ner_flair.py
+++ code/ners/ner_flair.py
@@ -1,45 +1,32 @@
-import os
-import json
-import re
-os.listdir()
-
-
 from flair.data import Sentence
 from flair.nn import Classifier
+import sys
+
+sys.path.append("..")
+
+from helpers.helper_cr import perform_cr
 
 
-f = "002"
-story_path = "nlp-course-skupina-123/data/stories/" + f + ".txt"
-story = open(story_path, "r").read()
+# story - text (whole story) (string)
+# use_cr - use coreference resolution (boolean)
+def ner_flair(story, use_cr=False):
+    tagger = Classifier.load("ner")
+
+    sentences = [s.strip() for s in re.split(r"(?<=[.?!])\s+", story)]
+    entities = set()
+    for s in sentences:
+        sentence = Sentence(s)
+        tagger.predict(sentence)
+        for entity in sentence.get_spans("ner"):
+            if entity.tag == "PER":
+                entities.add(entity.text)
+    return list(entities)
 
 
-sentences = [s.strip() for s in re.split(r'(?<=[.?!])\s+', story)]
-
-tagger = Classifier.load('ner')
-for s in sentences:
-  sentence = Sentence(s)
-  tagger.predict(sentence)
-  print(sentence)
-
-
-
-# annotation_path = "nlp-course-skupina-123/data/annotations/" + f + ".json"
-# a = json.load(open(annotation_path, "r"))
-# a['characters']
-
-stories_path = "nlp-course-skupina-123/data/stories"
-annotations_path = "nlp-course-skupina-123/data/annotations"
-
-# Loop through all files in the folder
-for story_name in os.listdir(folder_path):
-    # Check if the file has a .txt extension
-    if story_name.endswith('.txt'):
-        # Construct the full file path by joining the folder path and file name
-        story_path = os.path.join(stories_path, story_name)
-        annotation_path = os.path.join(annotations_path, story_name.split(".")[0], ".json")
-
-
-
-        story = open(story_path, "r").read()
-
-        annotation = json.load(open(annotation_path, "r"))
+def test_ner_flair():
+    text = "Joseph Robinette Biden Jr. is an American politician who is the 46th and\
+  current president of the United States. A member of the Democratic Party, \
+  he served as the 47th vice president from 2009 to 2017 under Barack Obama and\
+  represented Delaware in the United States Senate from 1973 to 2009."
+    print("Entities without CR", ner_flair(text))
+    print("Entities with CR", ner_flair(text, use_cr=True))
