8feb0308aa7d2f1bc5a1ea0364de541209287027
Nace Gorenc <nace.gorenc@outfit7.com>
2023-04-26 17:58:25 +0200

    kr neki

--- code/helpers/helper_cr.py
+++ code/helpers/helper_cr.py
@@ -5,5 +5,6 @@ from allennlp.predictors.predictor import Predictor
 model_url = "https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2020.02.27.tar.gz"
 predictor = Predictor.from_path(model_url)
 
+
 def perform_cr(story):
-  return predictor.coref_resolved(story)
+    return predictor.coref_resolved(story)
