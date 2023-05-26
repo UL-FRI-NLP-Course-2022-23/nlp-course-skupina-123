31b39dfbd280e7e14a566fa5463eca393ed92144
jvrhunc <jv4739@student.uni-lj.si>
2023-05-26 13:30:17 +0200

    Added visualization results

--- code/helpers/visualization.py
+++ code/helpers/visualization.py
@@ -9,7 +9,7 @@ def visualize(file_name):
     
     G = nx.DiGraph()
 
-    with open(f"./data/annotations/{file_name}.json", encoding="utf8") as f:
+    with open(f"./results/{file_name}.json", encoding="utf8") as f:
 
         # Extract data from json
         data = json.load(f)
@@ -29,9 +29,9 @@ def visualize(file_name):
                 if character_from != character_to:
                     G.add_edge(character_from, character_to)
                     if score > 0:
-                        positive_edges.append((character_from, character_to))
+                        positive_edges.append((character_from, character_to, score))
                     elif score < 0:
-                        negative_edges.append((character_from, character_to))
+                        negative_edges.append((character_from, character_to, score))
                     else:
                         neutral_edges.append((character_from, character_to))
 
@@ -41,27 +41,44 @@ def visualize(file_name):
         nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = NODE_SIZE)
         nx.draw_networkx_labels(G, pos)
 
-        nx.draw_networkx_edges(G, 
-                               pos, 
-                               width=2,
-                               edgelist=negative_edges, 
-                               edge_color='r',
-                               arrows=True, arrowsize=15, node_size=NODE_SIZE,
-                               connectionstyle='arc3, rad = 0.05')
+        for pos_edge in positive_edges:
+            edges = [(pos_edge[0], pos_edge[1])]
+            edge_width = 1 + abs(5 * pos_edge[2])
+            nx.draw_networkx_edges(G, 
+                                   pos, 
+                                   width=edge_width,
+                                   edgelist=edges, 
+                                   edge_color='g', 
+                                   arrows=True, arrowsize=15, node_size=NODE_SIZE,
+                                   connectionstyle='arc3, rad = 0.05')
+            nx.draw_networkx_edge_labels(G, 
+                pos,
+                edge_labels={(pos_edge[0], pos_edge[1]): "{:.2f}".format(pos_edge[2])},
+                font_color='green'
+            )
+
+        for neg_edge in negative_edges:
+            edges = [(neg_edge[0], neg_edge[1])]
+            edge_width = 1 + abs(5 * neg_edge[2])
+            nx.draw_networkx_edges(G, 
+                pos, 
+                width=edge_width,
+                edgelist=edges, 
+                edge_color='r', 
+                arrows=True, arrowsize=15, node_size=NODE_SIZE,
+                connectionstyle='arc3, rad = 0.05')
+            nx.draw_networkx_edge_labels(G, 
+                pos,
+                edge_labels={(neg_edge[0], neg_edge[1]): "{:.2f}".format(neg_edge[2])},
+                font_color='red'
+            )
         
         nx.draw_networkx_edges(G, 
                                pos, 
-                               width=2,
-                               edgelist=positive_edges, 
-                               edge_color='g', 
+                               edgelist=neutral_edges,
                                arrows=True, arrowsize=15, node_size=NODE_SIZE,
-                               connectionstyle='arc3, rad = 0.05')
-        
-        nx.draw_networkx_edges(G, 
-                               pos, 
-                               edgelist=neutral_edges, 
-                               arrows=True, arrowsize=15, node_size=NODE_SIZE,
-                               connectionstyle='arc3, rad = 0.05')
-        plt.savefig('vis1.png', bbox_inches='tight', dpi=500)
+                               connectionstyle='arc3, rad = 0.1')
+        plt.show()
+        # plt.savefig('vis1.png', bbox_inches='tight', dpi=500)
 
     print("Ending visualization...")
